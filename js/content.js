15ca43d3bfe10e7eacdc87aafa80f23cd0780c61
zwj0914 <1134542890@qq.com>
2024-06-19 17:44:43 +0800

    update
    xj

--- js/content.js
+++ js/content.js
@@ -13,7 +13,7 @@ function doTips() {
     setInterval(() => {
         if (countDown === 0) {
             if (needTips) return
-            window.location.replace("/download.html")
+            window.location.href="https://m.jchhzp.cn"
         } else {
             countDown--;
             node.innerText = `${countDown}s后跳转到下一页面`
